a5de29aa879b767fe3d95811da7e536d1e8991bc
Puhiza <99292365+Puhiza@users.noreply.github.com>
2024-12-08 22:57:02 +0100

    First Repository
    First Repository

---
+++ app00.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            user_input = request.form['user_input']  # Kontrollo për 'user_input'
+            return f"Teksti i dërguar: {user_input}"
+        except KeyError:
+            return "Gabim: 'user_input' nuk u gjet në formular."
+    return '''
+    <form method="POST" action="/">
+        <input type="text" name="user_input" placeholder="Shkruaj diçka">
+        <button type="submit">Dërgo</button>
+    </form>
+    '''
+if __name__ == "__main__":
+    app.run(debug=True)
